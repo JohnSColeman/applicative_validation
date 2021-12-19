381f77d10a69345b6e1991f26bdfe236f2e20499
John Coleman <qbyteconsulting@gmail.com>
2021-12-19 17:46:20 +0700

    add Validation type

--- lib/src/framework/datatypes/validated.dart
+++ lib/src/framework/datatypes/validated.dart
@@ -1,7 +1,7 @@
 part of applicative_validation_framework;
 
-/// Alias an Either with left type ArgumentError right validate value type A
+/// Alias for Either with left type ArgumentError right validate value type A
 typedef Validated<A> = Either<ArgumentError, A>;
 
-/// Alias for a function that returns a Validation given a NameValue
+/// Alias for function that returns a Validation given a NameValue
 typedef Validation<A> = Validated<A> Function(NameValue);
