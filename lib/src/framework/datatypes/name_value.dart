9f0a69231a4324e63d1a583de9bac58115c8e65d
John Coleman <qbyteconsulting@gmail.com>
2021-12-19 17:50:28 +0700

    add equality

--- lib/src/framework/datatypes/name_value.dart
+++ lib/src/framework/datatypes/name_value.dart
@@ -9,4 +9,15 @@ class NameValue<A> {
   String get name => _name;
 
   A get value => _value;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NameValue &&
+          runtimeType == other.runtimeType &&
+          _name == other._name &&
+          _value == other._value;
+
+  @override
+  int get hashCode => _name.hashCode ^ _value.hashCode;
 }
