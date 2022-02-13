e1ec76d7ddb0b95493258431c327037a0a39522f
John Coleman <qbyteconsulting@gmail.com>
2022-02-13 09:20:11 +0700

    refactor validator

--- examples/text_form_field_validation/lib/domain/validation/validator_specs.dart
+++ examples/text_form_field_validation/lib/domain/validation/validator_specs.dart
@@ -3,14 +3,13 @@ import 'package:applicative_validation/applicative_validation_specs.dart';
 
 final usernameValidator = Validator<String>(
     name: "username",
-    nullable: false,
     validation: [minLength(8), maxLength(15), noWhiteSpace(), alphanumeric()]);
 
 final passwordValidator = Validator<String>(
     name: "password",
-    nullable: false,
     validation: [minLength(8), maxLength(12), noWhiteSpace(), nonRepeating()]);
 
 final newPassword1Validator = passwordValidator.alias("newPassword1");
 
 final newPassword2Validator = passwordValidator.alias("newPassword2");
+
--- lib/src/framework/datatypes/validator.dart
+++ lib/src/framework/datatypes/validator.dart
@@ -5,40 +5,35 @@ part of applicative_validation_framework;
 /// A is the underlying type of the validated data
 /// name identifies the associated field - a language key or value for logging
 class Validator<A> {
-  final String _name;
-
-  final bool _nullable;
-
-  final Iterable<Validation<A>> _validation;
-
   /// this fields name
-  String get name => _name;
+  final String name;
 
-  /// true if this fields value is nullable (not required)
-  bool get nullable => _nullable;
+  /// true if a null value is valid for this field (value is optional)
+  final bool nullable;
 
-  Validator({required name, required validation, required nullable})
-      : _name = name,
-        _validation = validation,
-        _nullable = nullable;
+  /// a collection of validations to apply to a value for this field
+  final Iterable<Validation<A>> validation;
+
+  Validator(
+      {required this.name, required this.validation, this.nullable = false});
 
   /// returns a copy of this field with an alias name
   Validator<A> alias(String alias) =>
-      Validator<A>(name: alias, validation: _validation, nullable: _nullable);
+      Validator<A>(name: alias, validation: validation, nullable: nullable);
 
   /// returns a NameValue for this Validator with the given value
   NameValue<A> value(A value) => NameValue(name, value);
 
   /// returns a validated for this field given field value
   Validated<A> call(A? value) {
-    final Validated<A> required = (value != null || _nullable)
+    final Validated<A> required = (value != null || nullable)
         ? right(value)
         : left(ArgumentError.value(
             null, name, ErrorArgumentsBinding("required.err", {})));
     return value == null
         ? required
-        : required.flatMap((_) => _validation
-            .map((validator) => validator(NameValue(_name, value)))
+        : required.flatMap((_) => validation
+            .map((validator) => validator(NameValue(name, value)))
             .reduce((value, element) => value & element));
   }
 }
